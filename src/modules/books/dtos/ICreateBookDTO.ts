3e08631278398608a23ff6a0abb62483c5baf6f5
Victor Batista <victorbtst77@gmail.com>
2022-09-02 01:39:10 -0300

    fix: book
    Alterando tipagem da criação de livros

--- src/modules/books/dtos/ICreateBookDTO.ts
+++ src/modules/books/dtos/ICreateBookDTO.ts
@@ -1,10 +1,11 @@
 interface ICreateBookDTO {
+    id?: string;
     author: string;
     title: string;
     releaseYear: Date;
     rent: number;
     synopsis: string;
-    pages: string;
+    pages: number;
     publisher: string;
     isbn: string;
     bannerUrl: string;
