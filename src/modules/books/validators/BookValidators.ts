5b963e5eb37de6a60b888df8aebc7b994706edd8
Bioinfo_LBMB <projetobioinfo@gmail.com>
2022-09-15 17:11:58 -0300

    validators

--- src/modules/books/validators/BookValidators.ts
+++ src/modules/books/validators/BookValidators.ts
@@ -1,10 +1,7 @@
-const isbnNumbers = (isbn: unknown) => {
-    if (typeof (isbn) === 'string' || typeof (isbn) === 'number') return isbn.toString().replace(/[^0-9]/g, '');
-    return '';
-};
-
-export const validisbn = (isbn: string) : boolean => {
-
+export const validisbn = (isbn: unknown) : boolean => {
+if(!strCheck(isbn)){
+    return false;
+}
 const isbnnumbers = isbnNumbers(isbn);
 
 const regex = new RegExp('^(?:ISBN(?:-10)?:?\)?(?=[0-9X]{10}$|(?=(?:[0-9]+[-\]){3})[-\0-9X]{13}$)[0-9]{1,5}[-\]?[0-9]+[-\]?[0-9]+[-\]?[0-9X]$');
@@ -32,7 +29,45 @@ if(regex.test(isbnnumbers)){
     sum += ((last == 'X') ? 10 : (parseInt(last) - 0));
 
     {console.log('válido'); return (sum % 11 == 0);}
+    }
 }
 
-
+export const bookreleaseYear = (field : any) : boolean => {
+    if(objCheck(field)){
+        const year = new RegExp('^\d\d\d\d')
+        if(year.test(field.getFullYear)){return true;}
+        else {return false}
+    }
 }
+
+export const strCheck = (field : unknown) : boolean => {
+    if (typeof field === 'string'){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+export const boolCheck = (field : unknown) : boolean => {
+    if (typeof field === "string"){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+export const objCheck = (field : unknown) : boolean => {
+    if (typeof field === "object"){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+const isbnNumbers = (isbn: unknown) => {
+    if (typeof (isbn) === 'string' || typeof (isbn) === 'number') return isbn.toString().replace(/[^0-9]/g, '');
+    return '';
+};
